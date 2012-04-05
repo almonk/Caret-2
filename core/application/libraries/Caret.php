cddf65af5d66dd07e2b184b37382d043b4592905
Alasdair Monk <alasdair.monk@gmail.com>
2012-04-05 16:51:47 +0100

    Added new session env variables

--- core/application/libraries/Caret.php
+++ core/application/libraries/Caret.php
@@ -33,16 +33,19 @@ class Caret {
 
         // Create the site array globals
         $config = array(
-          'root' => base_url(),
-          'segments' => $CI->uri->segment_array(),
-          'current_url' => current_url()
+          'root' => base_url()
+        );
+
+        $session = array(
+            'segments' => $CI->uri->segment_array(),
+            'url' => current_url()
         );
         
         // Load the template using the yaml key 'template'
         $h2o = new h2o($theme_folder . 'templates/' . $page['template'] . '.html'); // load the template
 
         // Return the final html
-        return html_entity_decode($h2o->render(compact('page', 'config')));     
+        return html_entity_decode($h2o->render(compact('page', 'config', 'session')));     
 
 
     }
@@ -52,17 +55,22 @@ class Caret {
 
         $base_url = base_url();
 
-        // Adds page.url
-        $page_array['url'] = explode('.', $page_uri); // Remove .yaml from pageuri
+        // If it's the homepage, we know the url already
+        if ($page_uri == 'index.yaml') {
+            return base_url();
+        }else{
+            // Otherwise, we gotta figure it out
+            $page_array['url'] = explode('.', $page_uri); // Remove .yaml from pageuri
 
-        // If we get double slashes in the url, we need to remove them
-        if (substr($page_array['url'][0], 0, 1) == '/') {
-            $base_url = substr_replace(base_url() ,"",-1);
+            // If we get double slashes in the url, we need to remove them
+            if (substr($page_array['url'][0], 0, 1) == '/') {
+                $base_url = substr_replace(base_url() ,"",-1);
+            }
+
+            $page_array['url'] = $base_url . $page_array['url'][0]; // Grab first element of exploded array
+
+            return $page_array['url'];
         }
-
-        $page_array['url'] = $base_url . $page_array['url'][0]; // Grab first element of exploded array
-
-        return $page_array['url'];
     }
 
     public function find_page($uri){
