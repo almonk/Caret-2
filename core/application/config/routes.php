40b7fd9ed636fb04cb06f47350682b826efa0c40
Alasdair Monk <alasdair.monk@gmail.com>
2012-04-08 18:12:28 +0100

    Remove admin specific things

--- core/application/config/routes.php
+++ core/application/config/routes.php
@@ -1,13 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $route['default_controller'] = "page";
-
-if ($this->config->item('admin_enabled')) {
-	$route['admin'] = "admin";
-	$route['admin/page/(:any)'] = "admin/page/$1";
-	$route['admin/save/(:any)'] = "admin/save/$1";
-}
-
 $route[':any'] = "page";
 
 $route['404_override'] = '';
